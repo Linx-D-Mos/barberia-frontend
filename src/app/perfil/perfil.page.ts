1501296941c40435a94d6f2d5f5bf2086c21ab7f
HAguirre21 <126982406+HAguirre21@users.noreply.github.com>
2024-09-26 22:43:05 -0500

    cambiar o selecionar foto en el perfil
    instale nuevas librerias hagan " npm install "
    
    esta incompleto falta implementarla en el registros despues del codigo
    
    funciones
    -camara
    -galeria

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,23 +1,27 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { NavController, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonItem, IonLabel, IonButton, IonImg, IonSkeletonText } from '@ionic/angular/standalone';
+import { NavController, IonContent, IonHeader, IonTitle, IonToolbar, IonIcon, IonItem, IonLabel, IonButton, IonImg, IonSkeletonText, IonModal } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { arrowBackCircle, arrowBackCircleOutline, arrowBackOutline, logOutOutline, personCircleOutline } from 'ionicons/icons';
+import { arrowBackCircle, arrowBackCircleOutline, arrowBackOutline, logOutOutline, personCircleOutline, saveOutline, cameraOutline, imageOutline } from 'ionicons/icons';
 import { AuthService } from '../services/auth/auth.service';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
   standalone: true,
-  imports: [IonSkeletonText, IonImg, IonButton, IonLabel, IonItem, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonModal, IonSkeletonText, IonImg, IonButton, IonLabel, IonItem, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class PerfilPage implements OnInit {
+  capturedImage: string | undefined;
+
+
 
   constructor() { 
-    addIcons({personCircleOutline,logOutOutline,arrowBackOutline})
+    addIcons({arrowBackOutline,saveOutline,personCircleOutline,cameraOutline,imageOutline,logOutOutline});
   }
-
+  
   private authService = inject(AuthService)
   private nav = inject(NavController)
 
@@ -44,6 +48,93 @@ export class PerfilPage implements OnInit {
     this.nav.back();
   }
 
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Puedes usar Base64 o URI
+      source: CameraSource.Camera, // O usa CameraSource.Photos para la galería
+    });
+
+    // Aquí obtendrás la imagen en formato base64
+    //this.capturedImage = image.webPath // Usa webPath para mostrar la imagen
+  
+
+     this.capturedImage = image.webPath;
+
+     console.log(this.capturedImage)
+
+  //   // Subir la imagen al servidor
+  //   const formData = new FormData();
+  //   formData.append('file', this.dataURItoBlob(this.capturedImage), 'image.jpeg');
+
+  //   this.http.post('https://tu-servidor.com/upload', formData).subscribe(
+  //     (response) => {
+  //       console.log('Imagen subida con éxito');
+  //     },
+  //     (error) => {
+  //       console.log('Error al subir la imagen:', error);
+  //     }
+  //   );
+  // }
+
+  // // Convertir Base64 a Blob
+  // dataURItoBlob(dataURI: string) {
+  //   const byteString = atob(dataURI.split(',')[1]);
+  //   const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  //   const ab = new ArrayBuffer(byteString.length);
+  //   const ia = new Uint8Array(ab);
+
+  //   for (let i = 0; i < byteString.length; i++) {
+  //     ia[i] = byteString.charCodeAt(i);
+  //   }
+
+  //   return new Blob([ab], { type: mimeString });
+  // }
+  }
+
+  async openGallery(){
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Puedes usar Base64 o URI
+      source: CameraSource.Photos, // O usa CameraSource.Camera para la cámara
+      });
+
+      this.capturedImage = image.webPath;
+
+      //   this.capturedImage = image.dataUrl;
+
+  //   // Subir la imagen al servidor
+  //   const formData = new FormData();
+  //   formData.append('file', this.dataURItoBlob(this.capturedImage), 'image.jpeg');
+
+  //   this.http.post('https://tu-servidor.com/upload', formData).subscribe(
+  //     (response) => {
+  //       console.log('Imagen subida con éxito');
+  //     },
+  //     (error) => {
+  //       console.log('Error al subir la imagen:', error);
+  //     }
+  //   );
+  // }
+
+  // // Convertir Base64 a Blob
+  // dataURItoBlob(dataURI: string) {
+  //   const byteString = atob(dataURI.split(',')[1]);
+  //   const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+  //   const ab = new ArrayBuffer(byteString.length);
+  //   const ia = new Uint8Array(ab);
+
+  //   for (let i = 0; i < byteString.length; i++) {
+  //     ia[i] = byteString.charCodeAt(i);
+  //   }
+
+  //   return new Blob([ab], { type: mimeString });
+  // }
+
+      
+  }
   
 
 }
--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { addIcons } from 'ionicons';
-import { arrowBack, arrowBackOutline } from 'ionicons/icons';
+import { arrowBack, arrowBackOutline, cameraOutline, imageOutline } from 'ionicons/icons';
 import { NavController, IonicModule, ModalController } from '@ionic/angular'; // Importa solo IonicModule aquí
 import { AuthService } from '../services/auth/auth.service';
 import { RouterLink } from '@angular/router';
 import { VerificacionModalComponent } from '../verificar-correo-modal/verificar-correo-modal.component';
 import { InputOtpModule } from 'primeng/inputotp';
 import { ButtonModule } from 'primeng/button';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-registro',
@@ -27,12 +28,14 @@ import { ButtonModule } from 'primeng/button';
 })
 export class RegistroPage implements OnInit {
 
+  capturedImage: string | undefined;
+
   private authService = inject(AuthService);
   regisForm: any;
   email: any;
 
   constructor(private nav: NavController) {
-    addIcons({ arrowBack, arrowBackOutline });
+    addIcons({ arrowBack, arrowBackOutline,cameraOutline , imageOutline});
   }
 
   ngOnInit() {
@@ -91,4 +94,60 @@ export class RegistroPage implements OnInit {
         this.authService.showAlert(e?.error?.message);
     });
   }
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Puedes usar Base64 o URI
+      source: CameraSource.Camera, // O usa CameraSource.Photos para la galería
+    });
+
+    // Aquí obtendrás la imagen en formato base64
+    this.capturedImage = image.webPath; // Usa webPath para mostrar la imagen
+  }
+
+  async selectPicture() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri, // Puedes usar Base64 o URI
+      source: CameraSource.Camera, // O usa CameraSource.Photos para la galería
+    });
+
+       // Subir la imagen al servidor
+    //this.capturedImage = image.dataUrl;
+    //const formData = new FormData();
+    //formData.append('file', this.dataURItoBlob(this.capturedImage), 'image.jpeg');
+
+    //this.http.post('https://tu-servidor.com/upload', formData).subscribe(
+     // (response) => {
+      //  console.log('Imagen subida con éxito');
+     // },
+      //(error) => {
+      //  console.log('Error al subir la imagen:', error);
+     // }
+   // );
+
+    // Convertir Base64 a Blob
+  //dataURItoBlob(dataURI: string) {
+    //const byteString = atob(dataURI.split(',')[1]);
+    //const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    //const ab = new ArrayBuffer(byteString.length);
+    //const ia = new Uint8Array(ab);
+
+    //for (let i = 0; i < byteString.length; i++) {
+      //ia[i] = byteString.charCodeAt(i);
+    //}
+
+  //  return new Blob([ab], { type: mimeString });
+  //}
+
+    // Aquí obtendrás la imagen en formato base64
+    this.capturedImage = image.webPath; // Usa webPath para mostrar la imagen
+  }
+
+
+
+ 
 }
